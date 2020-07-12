1a55c0bd802171aae0f3912f59179978db62c6a0
Romario Gustave <romagustave10@gmail.com>
2020-07-12 20:54:19 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,12 +1,29 @@
-class PigLatinizer 
-  def piglatinize(user_phrase)
-    @new_word = user_phrase.split("")
-    vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
-    if vowels.include?(@new_word[0])
-      latinized_word = begins_with_vowel.join("")
-    else
-      latinized_word = begins_with_consonant.join("")
+require 'pry'
+
+class PigLatinizer
+
+  def piglatinize(word)
+    return word if %w[and an in].include?(word) #one syllable exceptions
+    letters = word.split("")
+    letters.keep_if {|letter| letter != "."}
+    if letters.size > 1
+      until vowel?(letters[0]) 
+        letters << letters.shift
+      end
+      letters  << "ay"
     end
-    latinized_word
-end 
+    letters.join
+  end
+
+  def to_pig_latin(text)
+    words = text.split(" ")
+    words.map! {|word| piglatinize(word)}
+    words.join(" ")
+  end
+
+  def vowel?(letter)
+    letter.downcase
+    letter == "o" || letter == "e" || letter == "a" || letter == "i" || letter == "u"
+  end
+
 end 
