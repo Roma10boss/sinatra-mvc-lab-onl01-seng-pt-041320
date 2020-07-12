bb2371078b3b445033d0d0acded3dcdeee75c0ff
Romario Gustave <romagustave10@gmail.com>
2020-07-12 20:42:05 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,11 +1,11 @@
 class PigLatinizer 
-  def piglanize(user_phrase)
-     vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+  def piglatinize(user_phrase)
+    @new_word = user_phrase.split("")
+    vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
     if vowels.include?(@new_word[0])
       latinized_word = begins_with_vowel.join("")
     else
       latinized_word = begins_with_consonant.join("")
     end
     latinized_word
-  end 
 end 
